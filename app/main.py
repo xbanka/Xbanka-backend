579736e2f726125142c4c60b3fb437dfeb9d6576
Joshua Oloton <olotonjoshua@gmail.com>
2025-09-30 15:53:23 +0100

    feat: configure cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.database import engine
 from app.api.v1.routes import api_version_one
 from app.models.base_model import Base
@@ -6,6 +7,15 @@ from app import models
 
 app = FastAPI()
 app.include_router(api_version_one)
+
+# Add the middleware to your FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
