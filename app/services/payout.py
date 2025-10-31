556568b48e41df0036bfc0ae0db818b34e6d3f22
Joshua Oloton <olotonjoshua@gmail.com>
2025-10-31 11:58:40 +0100

    fix(payout): prevent duplicate payment references by raising 400 HTTP error

--- app/services/payout.py
+++ app/services/payout.py
@@ -1,14 +1,23 @@
-from sqlalchemy.orm import Session
 from app.core.base.services import Service
 from app.models.payouts import Payout
 from app.schemas.payout import PayoutBase
 
 from datetime import datetime
-
+from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
 class PayoutService(Service):
     @staticmethod
     def create_new(db: Session, obj_in: PayoutBase, affiliate_id) -> Payout:
+
+        existing_ref = db.execute(
+            select(Payout).where(Payout.payment_ref == obj_in.payment_ref)
+        ).scalar_one_or_none()
+        
+        if existing_ref:
+            raise HTTPException(status_code=400, detail="Payment reference already exists.")
+
         new_payout = Payout(
             amount=obj_in.amount,
             bank=obj_in.bank,
