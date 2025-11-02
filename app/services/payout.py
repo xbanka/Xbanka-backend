79fd10c6e32009dbf7d05269cd9c3ce938aa4b88
Joshua Oloton <olotonjoshua@gmail.com>
2025-11-02 04:26:51 +0100

    feat: add check to prevent exceeding balance limit

--- app/services/payout.py
+++ app/services/payout.py
@@ -1,15 +1,24 @@
 from app.core.base.services import Service
+from app.models.customer import Customer
 from app.models.payouts import Payout
+from app.models.transactions import Transaction, TransactionStatusEnum
 from app.schemas.payout import PayoutBase
 
 from datetime import datetime
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.orm import Session
 
 class PayoutService(Service):
     @staticmethod
     def create_new(db: Session, obj_in: PayoutBase, affiliate_id) -> Payout:
+
+        available_balance = db.scalar(
+            select(func.coalesce(func.sum(Transaction.commission_amount), 0))
+            .join(Customer, Customer.id == Transaction.customer_id)
+            .where(Customer.affiliate_id == affiliate_id)
+            .where(Transaction.status == TransactionStatusEnum.approved)
+        ) or 0
 
         existing_ref = db.execute(
             select(Payout).where(Payout.payment_ref == obj_in.payment_ref)
@@ -17,6 +26,17 @@ class PayoutService(Service):
         
         if existing_ref:
             raise HTTPException(status_code=400, detail="Payment reference already exists.")
+        
+        try:
+            obj_in.amount = float(obj_in.amount)
+        except ValueError:
+            raise HTTPException(status_code=400, detail="Invalid amount format.")
+        
+        if obj_in.amount <= 0:
+            raise HTTPException(status_code=400, detail="Payout amount must be greater than zero.")
+        
+        if obj_in.amount > available_balance:
+            raise HTTPException(status_code=400, detail="Insufficient available balance for this payout.")
 
         new_payout = Payout(
             amount=obj_in.amount,
