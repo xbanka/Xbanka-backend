928bdf435ae6938b3ab31d0b5b30af71d6c99477
Joshua Oloton <olotonjoshua@gmail.com>
2025-11-23 14:54:22 +0100

    fix: resolve improper customer response schema

--- app/schemas/customer.py
+++ app/schemas/customer.py
@@ -26,8 +26,12 @@ class MockBase(BaseModel):
 class CustomerResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    message: str
-    customer: MockBase
+    id: UUID
+    first_name: str
+    first_name: str
+    email: str
+    phone_no: str
+    commission: float
 
 
 class CustomerCreateResponse(BaseModel):
