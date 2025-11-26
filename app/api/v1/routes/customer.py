a1d41868c4fa636c8f2fb21d5f3d51d9a7156292
Joshua Oloton <olotonjoshua@gmail.com>
2025-11-26 06:49:03 +0100

    fix: resolve error in creating new customer

--- app/api/v1/routes/customer.py
+++ app/api/v1/routes/customer.py
@@ -1,10 +1,8 @@
-from typing import Optional
-from app.core.enums import PayoutStatusEnum, TransactionStatusEnum
 from app.services.customer import CustomerService
 from app.db.database import get_db
 from app.schemas.customer import CustomerBase, CustomerCreateResponse
 
-from fastapi import APIRouter, status, Depends, Query, Response
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
 customer = APIRouter(prefix="/customers", tags=["Customers"])
 
@@ -13,6 +11,5 @@ customer = APIRouter(prefix="/customers", tags=["Customers"])
     status_code=status.HTTP_201_CREATED
 )
 def create_customer(customer_request: CustomerBase, db: Session = Depends(get_db)):
-    customer_service = CustomerService.create_customer(db, customer_request)
-    customer = customer_service.create_customer(customer_request)
+    customer = CustomerService.create_customer(db, customer_request)
     return {"message": "Customer created successfully", "customer": customer}
